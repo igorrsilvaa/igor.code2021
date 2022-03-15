dd8d5165904e84b3ee166eaeb80945f5096d35ae
Igor da Silva Francisco <68037768+igorrsilvaa@users.noreply.github.com>
2022-03-15 00:11:20 -0300

    CorDaLabel

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function mensagem(){
     alert("Espere pacientemente. Estou preparando meu novo portfólio para você, mas fique a vontade para acessar os demais links...")
 }
+
+function mensagem() {
+    document.getElementById
+}
